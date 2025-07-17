e13ce52e540d55b73b860ea5224ddb9bc3036701
Pedro Quessada <pedroluiz.quessada@gmail.com>
2025-07-17 17:50:00 -0300

    feat: estrutura inicial

---
+++ src/main/java/com/example/sistemaestoqueprecorabbitmq/SistemaEstoquePrecoRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemaestoqueprecorabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEstoquePrecoRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaEstoquePrecoRabbitmqApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/sistemaestoqueprecorabbitmq/connections/RabbitMQConnection.java
@@ -0,0 +1,59 @@
+package com.example.sistemaestoqueprecorabbitmq.connections;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitMQConnection {
+
+    @Value("${rabbitmq.exchange.nome}")
+    private String nomeExchange;
+
+    @Value("${rabbitmq.fila.estoque.nome}")
+    private String filaEstoqueNome;
+
+    @Value("${rabbitmq.fila.preco.nome}")
+    private String filaPrecoNome;
+
+    private final AmqpAdmin amqpAdmin;
+
+    public RabbitMQConnection(AmqpAdmin amqpAdmin) {
+        this.amqpAdmin = amqpAdmin;
+    }
+
+    private Queue fila(String nomeFila) {
+        return new Queue(nomeFila, true, false, false);
+    }
+
+    private DirectExchange trocaDireta() {
+        return new DirectExchange(nomeExchange);
+    }
+
+    private Binding relacionamento(Queue fila, DirectExchange trocaDireta) {
+        return new Binding(fila.getName(), Binding.DestinationType.QUEUE, trocaDireta.getName(), fila.getName(), null);
+    }
+
+    @PostConstruct
+    private void adiciona() {
+        Queue filaEstoque = this.fila(filaEstoqueNome);
+        Queue filaPreco = this.fila(filaPrecoNome);
+
+        DirectExchange troca = this.trocaDireta();
+
+        Binding ligacaoEstoque = this.relacionamento(filaEstoque, troca);
+        Binding ligacaoPreco = this.relacionamento(filaPreco, troca);
+
+        this.amqpAdmin.declareQueue(filaEstoque);
+        this.amqpAdmin.declareQueue(filaPreco);
+
+        this.amqpAdmin.declareExchange(troca);
+
+        this.amqpAdmin.declareBinding(ligacaoEstoque);
+        this.amqpAdmin.declareBinding(ligacaoPreco);
+    }
+}
